55b66e16be666d7b9a24d7a2cee9a1de2beaed87
NawafIT <nwef918@gmail.com>
2024-04-06 07:04:08 +0300

    done2

--- JS.js
+++ JS.js
@@ -1,20 +1,20 @@
 const euroCheck = document.getElementById('euroCheck');
-const rows = document.querySelectorAll('#standings tbody tr');
+const rows = document.querySelectorAll('#standings tbody tr')
 
 euroCheck.addEventListener('change', function() {
   if (this.checked) {
     let count = 0;
     rows.forEach(row => {
       if (count < 5) {
-        row.style.display = 'table-row';
+        row.style.display = 'table-row'
         count++;
       } else {
-        row.style.display = 'none';
+        row.style.display = 'none'
       }
-    });
+    })
   } else {
     rows.forEach(row => {
-      row.style.display = 'table-row';
-    });
+      row.style.display = 'table-row'
+    })
   }
-});
+})
